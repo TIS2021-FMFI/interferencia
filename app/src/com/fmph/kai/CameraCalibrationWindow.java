767e041f1212719ae4c6a867fedf3d91280eb0be
pureparadox <pavelnomer1@gmail.com>
2021-11-19 15:55:36 +0100

    Interference project base app: initial commit

---
+++ app/src/com/fmph/kai/CameraCalibrationWindow.java
@@ -0,0 +1,263 @@
+package com.fmph.kai;
+
+import javafx.application.Platform;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import org.opencv.calib3d.Calib3d;
+import org.opencv.core.*;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.videoio.VideoCapture;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import static org.opencv.videoio.Videoio.*;
+
+public class CameraCalibrationWindow extends Stage {
+    private final VideoCapture capture = new VideoCapture();
+    private ScheduledExecutorService timer;
+    private boolean isCalibrated = false;
+
+    // calibration parameters
+    private final Mat intrinsic = new Mat(3, 3, CvType.CV_32FC1);
+    private final Mat distCoeffs = new Mat();
+    private Image calibratedImage = null;
+    private final MatOfPoint3f obj = new MatOfPoint3f();
+    private final List<Mat> imagePoints = new ArrayList<>();
+    private final List<Mat> objectPoints = new ArrayList<>();
+    private MatOfPoint2f imageCorners = new MatOfPoint2f();
+    private Size size;
+
+    private int numRequired = 20;
+    private int numSnapshots = 0;
+    private int horSize = 9;
+    private int verSize = 6;
+
+    private Button btnTakeSnapshot;
+
+    public CameraCalibrationWindow(double x, double y, double width, double height) {
+        super();
+        this.setTitle("Camera calibration");
+
+        BorderPane pane = new BorderPane();
+
+        // TOP
+        HBox top = new HBox(10);
+        top.setPadding(new Insets(40, 40, 40, 40));
+        top.setAlignment(Pos.CENTER);
+        Label lblSelectCamera = new Label("Select camera: ");
+        SpinnerValueFactory<Integer> valueFactory = //
+                new SpinnerValueFactory.IntegerSpinnerValueFactory(-1, 3, 0);
+        Spinner<Integer> spnCamera = new Spinner<>();
+        spnCamera.setValueFactory(valueFactory);
+        Label lblBoards = new Label("Samples #");
+        TextField txtBoards = new TextField("20");
+        txtBoards.textProperty().addListener((obs, oldV, newV) -> {
+            if (!newV.isEmpty())
+                try {
+                    numRequired = Integer.parseInt(newV);
+                    if (btnTakeSnapshot != null)
+                        btnTakeSnapshot.setText("Take snapshot (" + numSnapshots + "/" + numRequired + ")");
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+                }
+        });
+        txtBoards.setMaxWidth(50);
+        Label lblHorizontalCorners = new Label("Horizontal corners #");
+        TextField txtHorizontalCorners = new TextField("9");
+        txtHorizontalCorners.textProperty().addListener((obs, oldV, newV) -> {
+            if (!newV.isEmpty())
+                try {
+                    horSize = Integer.parseInt(newV);
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+                }
+            updateSettings();
+        });
+        txtHorizontalCorners.setMaxWidth(50);
+        Label lblVerticalCorners = new Label("Vertical corners #");
+        TextField txtVerticalCorners = new TextField("6");
+        txtVerticalCorners.textProperty().addListener((obs, oldV, newV) -> {
+            if (!newV.isEmpty())
+                try {
+                    verSize = Integer.parseInt(newV);
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+                }
+            updateSettings();
+        });
+        txtVerticalCorners.setMaxWidth(50);
+        top.getChildren().addAll(lblSelectCamera, spnCamera, lblBoards, txtBoards, lblHorizontalCorners, txtHorizontalCorners, lblVerticalCorners, txtVerticalCorners);
+        pane.setTop(top);
+
+        // LEFT
+        ImageView imgNormal = new ImageView();
+        imgNormal.setFitWidth(width/2);
+        imgNormal.setPreserveRatio(true);
+        pane.setLeft(imgNormal);
+
+        // RIGHT
+        ImageView imgCalibrated = new ImageView();
+        imgCalibrated.setFitWidth(width/2);
+        imgCalibrated.setPreserveRatio(true);
+        pane.setRight(imgCalibrated);
+
+        // BOTTOM
+        HBox bottom = new HBox(10);
+        bottom.setPadding(new Insets(20, 20, 20, 20));
+        bottom.setAlignment(Pos.CENTER);
+        Button btnStartCamera = new Button("Start camera");
+        btnTakeSnapshot = new Button("Take snapshot");
+        btnTakeSnapshot.setDisable(true);
+        bottom.getChildren().addAll(btnStartCamera, btnTakeSnapshot);
+        pane.setBottom(bottom);
+
+        // SCENE
+        Scene scene = new Scene(pane, width, height);
+        this.setScene(scene);
+
+        this.setX(x);
+        this.setY(y);
+
+        // VideoCapture
+        btnStartCamera.setOnAction(e -> {
+            if (capture.isOpened()) {
+                btnStartCamera.setText("Start camera");
+                if (timer != null)
+                    timer.shutdown();
+                capture.release();
+                imgNormal.setImage(null);
+                imgCalibrated.setImage(null);
+            } else {
+                if (!capture.open(spnCamera.getValue() + CAP_DSHOW)) {
+                    System.out.println("Error opening camera!");
+                    return;
+                }
+                btnStartCamera.setText("Stop camera");
+                Runnable frameGrabber = () -> {
+                    Image imageToShow = grabFrame();
+                    Platform.runLater(() -> {
+                        imgNormal.setImage(imageToShow);
+                        imgCalibrated.setImage(calibratedImage);
+                    });
+                };
+                timer = Executors.newSingleThreadScheduledExecutor();
+                timer.scheduleAtFixedRate(frameGrabber, 0, 33, TimeUnit.MILLISECONDS);
+            }
+        });
+
+        // Calibration
+        updateSettings();
+        btnTakeSnapshot.setOnAction(e -> {
+            if (numSnapshots < numRequired) {
+                numSnapshots++;
+                imagePoints.add(imageCorners.clone());
+                imageCorners = new MatOfPoint2f();
+                objectPoints.add(obj.clone());
+                btnTakeSnapshot.setText("Take snapshot (" + numSnapshots + "/" + numRequired + ")");
+                if (numSnapshots == numRequired)
+                    calibrate();
+            } else {
+                calibrate();
+            }
+        });
+
+        this.setOnHiding(e -> {
+            if (timer != null)
+                timer.shutdown();
+            capture.release();
+        });
+    }
+
+    private Image grabFrame() {
+        if (capture.isOpened()) {
+            Mat frame = new Mat();
+            capture.read(frame);
+            if (isCalibrated)
+            {
+                Mat undistorted = new Mat();
+                Calib3d.undistort(frame, undistorted, intrinsic, distCoeffs);
+                calibratedImage = mat2Image(undistorted);
+            }
+            findAndDrawPoints(frame);
+            return mat2Image(frame);
+        }
+        return null;
+    }
+
+    private void findAndDrawPoints(Mat frame) {
+        Mat gray = new Mat();
+        Imgproc.cvtColor(frame, gray, Imgproc.COLOR_BGR2GRAY);
+        Size boardSize = new Size(horSize, verSize);
+        boolean found = Calib3d.findChessboardCorners(gray, boardSize, imageCorners, Calib3d.CALIB_CB_ADAPTIVE_THRESH + Calib3d.CALIB_CB_NORMALIZE_IMAGE + Calib3d.CALIB_CB_FAST_CHECK);
+        if (found) {
+            TermCriteria term = new TermCriteria(TermCriteria.EPS | TermCriteria.MAX_ITER, 30, 0.1);
+            Imgproc.cornerSubPix(gray, imageCorners, new Size(11, 11), new Size(-1, -1), term);
+            size = gray.size();
+            Calib3d.drawChessboardCorners(frame, boardSize, imageCorners, true);
+            btnTakeSnapshot.setDisable(false);
+        } else {
+            btnTakeSnapshot.setDisable(true);
+        }
+    }
+
+    private void calibrate() {
+        List<Mat> rvecs = new ArrayList<>();
+        List<Mat> tvecs = new ArrayList<>();
+        intrinsic.put(0, 0, 1);
+        intrinsic.put(1, 1, 1);
+        Calib3d.calibrateCamera(objectPoints, imagePoints, size, intrinsic, distCoeffs, rvecs, tvecs);
+        isCalibrated = true;
+
+        saveCalibration(intrinsic, distCoeffs);
+
+        btnTakeSnapshot.setDisable(true);
+    }
+
+    private Image mat2Image(Mat frame) {
+        MatOfByte buffer = new MatOfByte();
+        Imgcodecs.imencode(".png", frame, buffer);
+        return new Image(new ByteArrayInputStream(buffer.toArray()));
+    }
+
+    private void updateSettings() {
+        for (double j = 0; j < horSize * verSize; j++)
+            obj.push_back(new MatOfPoint3f(new Point3(j / horSize, j % verSize, 0.0f)));
+    }
+
+    private void saveCalibration(Mat intrinsic, Mat distCoeffs) {
+        saveDoubleMat(intrinsic, "intrinsic");
+        saveDoubleMat(distCoeffs, "distortion");
+    }
+
+    // TODO: loadDoubleMat?
+    private void saveDoubleMat(final Mat mat, final String fileName) {
+        final long count = mat.total() * mat.channels();
+        final double[] buff = new double[(int) count];
+        mat.get(0, 0, buff);
+        try (final DataOutputStream out = new DataOutputStream(new FileOutputStream(fileName))) {
+            for (double v : buff) {
+                out.writeDouble(v);
+            }
+        } catch (IOException e) {
+            // TODO: introduce an exception handling system
+            System.out.println("Error saving the matrix: " + e.getMessage());
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText("Error saving the calibration!");
+            alert.setContentText(e.getMessage());
+            alert.showAndWait();
+        }
+    }
+}
---
+++ app/src/com/fmph/kai/InterferenceApplication.java
@@ -0,0 +1,126 @@
+package com.fmph.kai;
+
+import javafx.application.Application;
+import javafx.geometry.Orientation;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import org.opencv.core.Core;
+
+import java.io.File;
+
+public class InterferenceApplication extends Application {
+    static { System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
+
+    private final double width = 1366;
+    private final double height = 768;
+
+    private Group root;
+    private Stage stage;
+    private Scene scene;
+
+    private double lineSize;
+
+    @Override
+    public void start(Stage stage) {
+        System.out.println("Using OpenCV v" + Core.VERSION);
+        System.out.println("Using JavaFX v" + com.sun.javafx.runtime.VersionInfo.getVersion());
+        this.stage = stage;
+        root = new Group();
+        scene = new Scene(root, width, height);
+        initializeGUI();
+        stage.setTitle("Interference analyzer");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private void initializeGUI() {
+        // Menu Bar
+        MenuBar menu = new MenuBar();
+        Menu fileMenu = new Menu("File");
+        Menu editMenu = new Menu("Edit");
+        MenuItem openMenuItem = new MenuItem("Open");
+        MenuItem setLineSizeMenuItem = new MenuItem("Line size");
+        MenuItem resetLineMenuItem = new MenuItem("Reset line");
+        MenuItem cameraCalibrationMenuItem = new MenuItem("Camera calibration");
+        fileMenu.getItems().addAll(openMenuItem, cameraCalibrationMenuItem);
+        editMenu.getItems().addAll(setLineSizeMenuItem, resetLineMenuItem);
+        menu.getMenus().addAll(fileMenu, editMenu);
+
+        // Split Pane
+        SplitPane splitPane = new SplitPane();
+        splitPane.setLayoutY(25);
+        splitPane.orientationProperty().setValue(Orientation.HORIZONTAL);
+        ImageCanvas imageCanvas = new ImageCanvas(width/2, height - 25);
+
+        // Graph
+        NumberAxis xAxis = new NumberAxis();
+        NumberAxis yAxis = new NumberAxis();
+        xAxis.setLabel("X");
+        yAxis.setLabel("Y");
+        LineChart<Number, Number> lineChart = new LineChart<>(xAxis, yAxis);
+        lineChart.setMaxWidth(width/2);
+
+        splitPane.getItems().addAll(imageCanvas, lineChart);
+
+        // Actions
+        openMenuItem.setOnAction(e -> {
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Choose a picture");
+            fileChooser.setInitialDirectory(
+                    new File(System.getProperty("user.home"))
+            );
+            fileChooser.getExtensionFilters().addAll(
+                    new FileChooser.ExtensionFilter("All Images", "*.*"),
+                    new FileChooser.ExtensionFilter("JPG", "*.jpg"),
+                    new FileChooser.ExtensionFilter("PNG", "*.png")
+            );
+            File file = fileChooser.showOpenDialog(stage);
+            if (file != null) {
+                imageCanvas.setImage(new Image(file.toURI().toString()));
+            }
+        });
+
+        imageCanvas.setOnMouseClicked(e -> {
+            if (imageCanvas.click(e.getX(), e.getY())) {
+                lineChart.getData().clear();
+                XYChart.Series<Number, Number> series = new XYChart.Series<>();
+                series.setName("Sinusoid");
+                for (double x = 0; x < 5*Math.PI; x += Math.PI/24) {
+                    series.getData().add(new XYChart.Data<>(x, Math.sin(x)));
+                }
+                lineChart.getData().add(series);
+            }
+        });
+
+        setLineSizeMenuItem.setOnAction(e -> {
+            TextInputDialog tid = new TextInputDialog();
+            tid.setHeaderText("Enter new line size:");
+            tid.setOnHidden(event -> {
+                lineSize = Double.parseDouble(tid.getEditor().getText());
+            });
+            tid.show();
+        });
+
+        resetLineMenuItem.setOnAction(e -> {
+            imageCanvas.resetLine();
+        });
+
+        cameraCalibrationMenuItem.setOnAction(e -> {
+            CameraCalibrationWindow cameraCalibrationWindow = new CameraCalibrationWindow(stage.getX() + 20, stage.getY() + 20, width - 40, height - 40);
+            cameraCalibrationWindow.show();
+        });
+
+        root.getChildren().addAll(menu, splitPane);
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
